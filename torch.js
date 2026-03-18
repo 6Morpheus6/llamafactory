78a781631c939d34e4e99cab31bf0c03bf82389b
6Morpheus6 <wanton-decay@gmx.net>
2026-03-18 22:49:34 +0100

    update flash attention

--- torch.js
+++ torch.js
@@ -10,7 +10,7 @@ module.exports = {
         "path": "{{args && args.path ? args.path : '.'}}",
         "message":[
           "uv pip install torch==2.7.0 torchvision==0.22.0 torchaudio==2.7.0 {{args && args.xformers ? 'xformers' : ''}} --index-url https://download.pytorch.org/whl/cu128",
-          "uv pip install https://huggingface.co/lldacing/flash-attention-windows-wheel/resolve/main/flash_attn-2.7.4.post1%2Bcu128torch2.7.0cxx11abiFALSE-cp310-cp310-win_amd64.whl"
+          "uv pip install https://github.com/mjun0812/flash-attention-prebuild-wheels/releases/download/v0.4.10/flash_attn-2.7.4+cu128torch2.7-cp311-cp311-win_amd64.whl"
         ]
       }
     },
@@ -68,8 +68,7 @@ module.exports = {
         "path": "{{args && args.path ? args.path : '.'}}",
         "message": [
           "uv pip install torch==2.7.0 torchvision==0.22.0 torchaudio==2.7.0 {{args && args.xformers ? 'xformers' : ''}} --index-url https://download.pytorch.org/whl/cu128",
-          "uv pip install packaging ninja",
-          "uv pip install flash-attn --no-build-isolation"
+          "uv pip install https://github.com/mjun0812/flash-attention-prebuild-wheels/releases/download/v0.7.2/flash_attn-2.7.4+cu128torch2.7-cp311-cp311-linux_x86_64.whl"
         ]
       }
     },
